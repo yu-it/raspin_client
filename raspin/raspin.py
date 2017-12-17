ecd80319961aa37932b12afb57781cadda057faf
fwje7971 <fwje7971@hotmail.com>
2017-12-17 23:43:28 +0900

    今日までのところコミット

--- raspin/raspin.py
+++ raspin/raspin.py
@@ -2,24 +2,43 @@
 
 import urllib2
 import json
-
+import requests
 class api:
 
     #serverにはサーバ名またはIP,portにはポート番号
     def __init__(self, server, port):
         #http://localhost:3000
-        self.api_template = "http://" + server + ":" + str(port) + "/raspin-api/{query}"
+        self.api_template = "http://" + server + ":" + str(port) + "/raspin/internal{query}"
+        self.current_machine = ""
+        self.current_process = ""
+        self.current_if = ""
         self.ping()
 
-    def __call_api_get(self, path):
+    def __api_url(self, path):
 
         return self.__http_get(self.api_template.format(query = path))
 
     def __http_get(self, url):
         self.__log (url)
-        r = urllib2.urlopen(url)
-        res = r.read()
-        return json.loads(res)
+        r = requests.get(url)
+        return [r.status_code, r.json()]
+    def __http_delete(self, url):
+        self.__log (url)
+        r = requests.delete(url)
+        return [r.status_code, r.json()]
+
+    def __http_put(self, url, json = None):
+        self.__log (url)
+        if (json is None) :
+            r = requests.put(url,
+                                    headers={'content-type': 'text/plain'},
+                                    )
+        else:
+            r = requests.put(url,
+                                    data=json,
+                                    headers={'content-type': 'text/plain'},
+                                    )
+        return [r.status_code, r.json()]
 
     def __log(self, message):
         print(message)
@@ -33,6 +52,88 @@ class api:
             self.__log(message)
             raise Exception(message)
 
+    def machines_url(self):
+        return self.api_template.format(query="/machines")
+    def build_error_message(self, res, url, method):
+        return "status:{status}, response:{response}, method:{method}, url:{url}".format(status = res[0],response = res[1],method = method, url=url)
+    def machines(self):
+        try:
+            res = self.__http_get(self.machines_url())
+            if res[0] > 300:
+                raise Exception("ステータスが{status}".format())
+        except Exception as ex:
+            raise Exception(ex)
+
+    def machine_url(self,machine):
+        return self.machines_url() + "/" + machine
+
+    def get_machine(self, machine = None):
+        if machine is None:
+            machine = self.current_machine
+        try:
+            self.__http_get(self.machine_url(machine))
+            self.current_machine = machine
+        except Exception as ex:
+            raise Exception(ex)
+
+    def put_machine(self, machine = "MyMachine", machine_disp_name = "MyMachine"):
+        try:
+
+            self.__http_get(self.machine_url(machine) + "?machine_disp_name=" + machine_disp_name)
+            self.current_machine = machine
+        except Exception as ex:
+            raise Exception(ex)
+
+    def processes_url(self,machine):
+        return self.machine_url(machine) + "/processes"
+
+    def processes(self, machine=None):
+        if machine is None:
+            machine = self.current_machine
+        try:
+            self.__http_get(self.processes_url(machine) + "/processes")
+            self.current_machine = machine
+        except Exception as ex:
+            raise Exception(ex)
+
+    def process_url(self,machine, process):
+        return self.processes_url(machine) + "/" + process
+
+
+    def get_process(self, process=None, machine=None):
+        if process is None:
+            process = self.current_process
+        if machine is None:
+            machine = self.current_machine
+        try:
+            self.__http_get(self.process_url(machine, process))
+            self.current_machine = machine
+        except Exception as ex:
+            raise Exception(ex)
+
+    def put_process(self, process_id, process_disp_name=None, machine = None):
+        if machine is None:
+            machine = self.current_machine
+        if process_disp_name is None:
+            process_disp_name = process_id
+        try:
+            self.__http_get(self.process_url(process_id) + "?process_disp_name=" + process_disp_name)
+            self.current_process = process_id
+        except Exception as ex:
+            raise Exception(ex)
+
+
+
+
+
+
+
+
+
+
+
+
+                    #以下、レガシー
     def subscribe_control_message(self, pvid, timeout_second):
         query = 'SubscribeControlMessage?pvid={pvid}'.format(pvid = pvid)
         try:
