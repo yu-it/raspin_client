b47b64045a80f6aca30f067f131ef7f3979dcde5
yu-it <yousuke0303@gmail.com>
2018-01-02 22:08:18 +0900

    com

--- raspin/ssetest.py
+++ raspin/ssetest.py
@@ -2,7 +2,7 @@ import sseclient
 import requests
 
 import threading
-res = sseclient.SSEClient(requests.get("http://192.168.2.123:3000/raspin-ut/sse/receive", stream=True))
+res = sseclient.SSEClient(requests.get("http://localhost:3000/raspin-ut/sse/receive", stream=True))
 
 for event in res.events():
     print("get event:" + event.data)
